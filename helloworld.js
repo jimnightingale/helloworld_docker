b0afd648978d3196eee983482c915dd1ce1238b2
jimnightingale <nightingalejames@yahoo.co.uk>
2017-04-26 10:47:08 +0100

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,34 +2,53 @@ var http = require('http');
 var os = require('os');
 var hostname = os.hostname();
 
+var listeningPort = 80;
+var hitCountTotal = 0;
+//var hitCountHW = 0;
+
 var server = http.createServer(function (req, res) {
   //don't waste time with favicons
   if (req.url != "/favicon.ico") {
-    var date = new Date().toISOString();
-    var name = "World"
-    if (req.url != "/") {
-      name = req.url.split("/")[1];
-    }
     
-    // respond to the request
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello " + name + "\n");
-    res.write("from bet365@" + hostname + "\n");
-    res.write("at " + date + "\n");
-    res.end();
-    
-    // do some logging
-    var clientIp = null;
-    var forwardedIps = req.headers['x-forwarded-for'];
-    if (forwardedIps) {
-      clientIp = forwardedIps[0];
+    if (req.url == "/metrics") {
+      // /metrics for Prometheus monitoring
+      res.writeHead(200, {"Content-Type": "text/plain; version=0.0.4"});
+      res.write("# HELP hit_count_total The total amount of hits to this service exluding /favicon and /metrics." + "\n");
+      res.write("# TYPE hit_count_total counter" + "\n");
+      res.write("hit_count_total: " + hitCountTotal + "\n");
+      //res.write("# HELP hit_count_hw The amount of unparametised hits (ie returns 'Hello World' to this service exluding /favicon and /metrics ." + "\n")
+      //res.write("hit_count_hw: " + hitCountHW + "\n");
+      res.end();  
     } else {
-      clientIp = req.connection.remoteAddress;
+      var date = new Date().toISOString();
+      var name = "World"
+      if (req.url != "/") {
+        name = req.url.split("/")[1];
+      } else {
+        hitCountHW ++;
+      }
+      
+      // respond to the request
+      res.writeHead(200, {"Content-Type": "text/plain"});
+      res.write("Hello " + name + "\n");
+      res.write("from bet365@" + hostname + "\n");
+      res.write("at " + date + "\n");
+      res.end();
+
+      // do some logging
+      var clientIp = null;
+      var forwardedIps = req.headers['x-forwarded-for'];
+      if (forwardedIps) {
+        clientIp = forwardedIps[0];
+      } else {
+        clientIp = req.connection.remoteAddress;
+      }
+      hitCountTotal++
+      console.log(date + "\t" + clientIp + "\t" + hitCountTotal + "\t" + name + "\t" + req.url);
     }
-    console.log(date + "\t" + clientIp + "\t" + name + "\t" + req.url);
   }
 });
 
-server.listen(80);
+server.listen(listeningPort );
 
-console.log("Server running at http://localhost:80/");
+console.log("Server running at http://localhost:" + listeningPort + "/");
